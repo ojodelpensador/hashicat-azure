aff568cc0b255ee664028634673fd762076cdd0e
Bruce Bawcom <ojodelpensador@gmail.com>
2021-05-13 19:26:03 +0000

    Update version add other vars

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,9 @@
 module "network" {
   source  = "app.terraform.io/bbawcom-training/network/azurerm"
-  version = "3.3.0"
+  version = "3.0.1"
   # insert required variables here
   resource_group_name = azurerm_resource_group.myresourcegroup.name
+  address_space       = "10.0.0.0/16"
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
 }
